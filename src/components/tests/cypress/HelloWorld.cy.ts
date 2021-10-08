1388bc98fea03557e8f96ca005e2d90fb24e0b9e
Levi Brown <changinglanes@gmail.com>
2021-10-08 10:44:07 -0700

    add basic cypress test

---
+++ src/components/tests/cypress/HelloWorld.cy.ts
@@ -0,0 +1,8 @@
+import { mount } from '@cypress/vue'
+import HelloWorld from '../../HelloWorld.vue';
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    mount(HelloWorld)
+  })
+})
