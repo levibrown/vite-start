5f81c9981a330b91af47f9751c9670f347b0b5a3
Levi Brown <changinglanes@gmail.com>
2021-10-09 09:55:18 -0700

    trying to test emits

--- src/components/tests/cypress/HelloWorld.cy.ts
+++ src/components/tests/cypress/HelloWorld.cy.ts
@@ -2,7 +2,49 @@ import { mount } from '@cypress/vue'
 import HelloWorld from '../../HelloWorld.vue';
 
 describe('HelloWorld', () => {
-  it('renders properly', () => {
+  it('renders a message', () => {
+    const msg = 'Hello Cypress Component Testing!'
+    mount(HelloWorld, {
+      propsData: {
+        msg
+      }
+    }).get('h1')
+      .should('have.text', msg)
+  })
+
+  it('updates button text', () => {
     mount(HelloWorld)
+      .get('button')
+      .should('have.text', 'count is: 0')
+      .click()
+      .should('have.text', 'count is: 1')
+  })
+
+  it('tests', () => {
+    // this doesn't work
+    // mount(HelloWorld)
+    // const spy = cy.spy()
+    // Cypress.vue.$on('click', spy)
+    // cy.get('button')
+    //   .click()
+    //   .then(() => {
+    //     expect(spy).to.be.calledOnce
+    //   })
+
+    // this either
+    // mount(HelloWorld)
+    //   .get('button')
+    //   .click()
+    //   .vue()
+    //   .then((wrapper) => {
+    //     debugger
+    //   })
+
+    mount(HelloWorld)
+    cy.get('button').click()
+    cy.vue().then((wrapper) => {
+      debugger
+    })
+
   })
 })
